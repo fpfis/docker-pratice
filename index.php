4ae2552716c72fb078de7c3c8e227037d64a72ed
EC2 Default User <ec2-user@ip-172-31-7-74.eu-west-1.compute.internal>
2018-03-08 16:07:06 +0000

    Repo Updated

--- index.php
+++ index.php
@@ -4,10 +4,9 @@ require __DIR__.'/vendor/autoload.php';
 
 
 use Symfony\Component\Yaml\Yaml;
-
 $array = array(
     'foo' => 'bar',
-    'bar' => array('foo' => 'bar', 'bar' => 'baz' => 'test'),
+    'bar' => array('foo' => 'bar', 'bar' => 'baz'),
 );
-
-echo Yaml::dump($array);
+echo Yaml::
+dump($array);
