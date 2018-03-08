5ccfa8e90b2448b37f20a7ee46f5d8ee8cb616a4
fmadmin <tiago.mauricio@findmore.pt>
2018-03-08 16:12:53 +0000

    Treeeeeeeeeees 2

--- index.php
+++ index.php
@@ -13,10 +13,14 @@ $array = array(
 echo Yaml::dump($array);
 $a=0;
 $tree='*';
+$start_time = time();
 while (1) {
     for ($a; $a>0; $a--){
     	$tree = $tree.'*';
     }
     print($tree."\n");
     $a++;
+    if ((time() - $start_time) > 900) {
+      return false; // timeout, function took longer than 300 seconds
+    }
 }
