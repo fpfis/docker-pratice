9ab73144a0c282a9654aa949b7896809d0900918
fmadmin <tiago.mauricio@findmore.pt>
2018-03-08 16:03:59 +0000

    Treeeeeeeeeees

--- index.php
+++ index.php
@@ -7,7 +7,16 @@ use Symfony\Component\Yaml\Yaml;
 
 $array = array(
     'foo' => 'bar',
-    'bar' => array('foo' => 'bar', 'bar' => 'baz' => 'test'),
+    'bar' => array('foo' => 'bar', 'bar' => 'baz'),
 );
 
 echo Yaml::dump($array);
+$a=0;
+$tree='*';
+while (1) {
+    for ($a; $a>0; $a--){
+    	$tree = $tree.'*';
+    }
+    print($tree."\n");
+    $a++;
+}
