9128f00dfe7c506e142dff04e0a7f55f22728bad
EC2 Default User <ec2-user@ip-172-31-25-66.eu-west-1.compute.internal>
2018-03-08 16:04:19 +0000

    secobau1

--- index.php
+++ index.php
@@ -1,13 +1,1 @@
-<?php
-
-require __DIR__.'/vendor/autoload.php';
-
-
-use Symfony\Component\Yaml\Yaml;
-
-$array = array(
-    'foo' => 'bar',
-    'bar' => array('foo' => 'bar', 'bar' => 'baz' => 'test'),
-);
-
-echo Yaml::dump($array);
+<?php phpinfo();
